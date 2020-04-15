d131bc2edeb2ac5413bf2b39f640eb087d0cb3ec
Vincent Agnano <vinyll@protonmail.com>
2020-04-15 12:41:17 +0200

    basic log into file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$ip = explode('.', $_SERVER['REMOTE_ADDR']);
+
+$parts = [
+  date("Y-m-d H:i:s"),
+  "{$ip[0]}.{$ip[1]}.0.0",
+  $_SERVER['HTTP_REFERER'],
+  $_POST['url'],
+  $_POST['action'],
+  $_POST['name'],
+  "\n"
+];
+file_put_contents('stats.log', join("\t", $parts), FILE_APPEND);
+
